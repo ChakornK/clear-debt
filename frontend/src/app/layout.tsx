c0475d9082f1a5faa95cc61d37e7ff326c80c533
ChakornK <chakornk2007@gmail.com>
2026-02-28 15:30:50 -0800

    feat: add font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,6 +12,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
