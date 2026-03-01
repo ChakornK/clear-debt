4f70bffd2c27f331cf9ded3977fda28019a867c3
Ellie Neufeld <ellie_neufeld@sfu.ca>
2026-02-28 20:51:19 -0800

    feat: update image

--- frontend/src/components/PrimaryButton.tsx
+++ frontend/src/components/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { TbCalendarEvent } from "react-icons/tb";
 
 interface PrimaryButtonProps {
     children: any;
@@ -7,12 +7,10 @@ interface PrimaryButtonProps {
 
 const PrimaryButton = ({ children, onClick }: PrimaryButtonProps) => {
   return (
-    <button type="button" onClick={onClick} className="font-bold px-4 py-3 items-center justify-center gap-3 rounded-xl h-14 bg-white bg-white border-2 border-green-200 transition-all">
+    <button type="button" onClick={onClick} className="font-bold px-4 py-3 items-center justify-center gap-3 rounded-xl h-14 bg-white hover:bg-red border-2 border-green-200 transition">
       <div className="flex flex-row gap-2 items-center">
         <div className="text-green-400">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-          </svg>
+          <TbCalendarEvent />
         </div>
         {children}
       </div>
