7c6ebf20bda1273e2abfbead04f6fe13a1849d72
Ellie Neufeld <ellie_neufeld@sfu.ca>
2026-03-01 09:16:09 -0800

    feat: remove extra arrow icon

--- frontend/src/components/DebtInputField.tsx
+++ frontend/src/components/DebtInputField.tsx
@@ -66,9 +66,6 @@ export const DebtInputField = ({ id, debt, onRemoveClick, onFieldChange, showErr
               ))}
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-slate-400">
-              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
             </div>
           </div>
         </div>
@@ -136,9 +133,6 @@ export const DebtInputField = ({ id, debt, onRemoveClick, onFieldChange, showErr
               ))}
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-slate-400">
-              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
             </div>
           </div>
         </div>
